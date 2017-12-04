ca1a140e09a5afb09ffd98cc25299bd5bd50205a
marco <marco.cardinali@gmail.com>
2017-12-04 18:39:46 +0100

    prova deploy automatico

--- src/main/java/it/cardinals/main/controller/SampleController.java
+++ src/main/java/it/cardinals/main/controller/SampleController.java
@@ -13,7 +13,7 @@ public class SampleController {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        return "Hello Worlddddddddddddddddddddd!";
     }
 
     public static void main(String[] args) throws Exception {
