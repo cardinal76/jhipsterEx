3639db4e4d2ea07645ed28681530cd31bafae29f
marco <marco.cardinali@gmail.com>
2017-12-05 16:39:25 +0100

    banner

---
+++ src/main/java/it/cardinals/main/config/AppConfiguration.java
@@ -0,0 +1,31 @@
+package it.cardinals.main.config;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfiguration {
+	@Value("${DATABASE_URL}")
+	private String dbUrl;
+	
+	@Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(dbUrl);
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
+}
